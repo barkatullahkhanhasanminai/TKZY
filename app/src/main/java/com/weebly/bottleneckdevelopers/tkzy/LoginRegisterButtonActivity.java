4e6632cb847e62126c31fb05430d197c5fb16f28
barkatullahkhanhasanminai <barkat1996khan@gmail.com>
2018-04-17 17:39:47 +0530

    Initial Commit

--- app/src/main/java/com/weebly/bottleneckdevelopers/tkzy/LoginRegisterButtonActivity.java
+++ app/src/main/java/com/weebly/bottleneckdevelopers/tkzy/LoginRegisterButtonActivity.java
@@ -1,7 +1,7 @@
 package com.weebly.bottleneckdevelopers.tkzy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
 public class LoginRegisterButtonActivity extends AppCompatActivity {
